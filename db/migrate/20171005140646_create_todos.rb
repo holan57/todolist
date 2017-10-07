3861a0ef5d3958d8c185e61fb17af373508d904d
holan57 <hoogan57@gmail.com>
2017-10-07 21:25:02 +0800

    first upload

---
+++ db/migrate/20171005140646_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.date :duedate
+      t.text :note
+      t.text :detail
+      t.timestamps
+    end
+  end
+end
